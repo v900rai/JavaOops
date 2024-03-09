f9c4bc7d58a62cb56ec50a2371aa7b72788ae544
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-09 14:10:02 +0530

    Add files via upload

---
+++ OnlyReturnHello.java
@@ -0,0 +1,14 @@
+package JavaOops;
+
+public class OnlyReturnHello {
+    public static void main(String[] args){
+        String str=m1();
+        System.out.println(str);
+
+    }
+
+    public static String m1() {
+        return "vipul rai";
+    }
+
+}
