c1703f488145e783ae3bb862d190311269a37910
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-09 14:09:32 +0530

    Add files via upload

---
+++ AllCharacterMethod.java
@@ -0,0 +1,19 @@
+package JavaOops;
+
+public class AllCharacterMethod {
+    public static void main(String []args){
+        float f1=m1(101, "vishalrai",'A',12.2,true);
+        System.out.println(f1);
+
+    }
+    public static float m1(int a, String b, char c, double d,boolean b1){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(b1);
+        return 12.2f;
+
+    }
+
+}
