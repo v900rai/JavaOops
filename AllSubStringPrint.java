0a1f17d4dd214dbf5d3da7d4d601d93424a9196e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-10 19:38:20 +0530

    Add files via upload

---
+++ AllSubStringPrint.java
@@ -0,0 +1,12 @@
+package String;
+
+public class AllSubStringPrint {
+    public static void main(String[] args) {
+        String s="abcd";
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<=s.length(); j++){
+                System.out.println(s.substring(i,j));
+            }
+        }
+    }
+}
