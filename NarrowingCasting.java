18c2446083085dbfffc921bf743f4570ac213074
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-29 18:05:11 +0530

    Add files via upload

---
+++ NarrowingCasting.java
@@ -0,0 +1,11 @@
+package JavaOops.collections;
+
+public class NarrowingCasting {
+    public static void main(String []args){
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
+    }
+}
